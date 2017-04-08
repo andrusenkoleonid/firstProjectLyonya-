c29c5a8375e5a5acca74aedeecddb6bb6d9b2034
Andrusenko Leonid <andrusenoleonid@yandex.ru>
2017-04-08 14:44:40 +0300

    second commit

--- gitTestProject/src/main/java/com/HelloWorld.java
+++ gitTestProject/src/main/java/com/HelloWorld.java
@@ -6,5 +6,5 @@ package com;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!!!");
-    }
-}
+   	System.out.println("Hello, world2");
+}	
