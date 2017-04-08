ff24597641488bcd3f92a7a0f6b7f9775d8fd1e5
Andrusenko Leonid <andrusenoleonid@yandex.ru>
2017-04-08 14:37:41 +0300

    first commit

---
+++ gitTestProject/src/main/java/com/HelloWorld.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by Acer on 08.04.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!!!");
+    }
+}
